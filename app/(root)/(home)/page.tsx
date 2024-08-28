e5b7722575d425c737b1aa6eb231f4cfa868673f
Derek Kahongo <derek.elijah@gmail.com>
2024-08-28 20:16:04 +0100

    ask_question implementation

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -5,66 +5,14 @@ import NoResult from "@/components/shared/NoResult";
 import LocalSearchbar from "@/components/shared/search/LocalSearchbar";
 import { Button } from "@/components/ui/button";
 import { HomePageFilters } from "@/constants/filters";
+import { getQuestions } from "@/lib/actions/question.action";
 import Link from "next/link";
 
-const questions = [
-  {
-    _id: "1",
-    title: "Cascading Deletes in SQLAlchemy?",
-    tags: [
-      { _id: "1", name: "python" },
-      { _id: "2", name: "sql" },
-    ],
-    author: {
-      _id: "1",
-      name: "John Doe",
-      picture: "https://example.com/johndoe.jpg",
-    },
-    upvotes: 2500,
-    views: 10000,
-    answers: [
-      {
-        _id: "1",
-        content: "You can use the cascade='all, delete-orphan' option.",
-        author: {
-          _id: "2",
-          name: "Jane Smith",
-          picture: "https://example.com/janesmith.jpg",
-        },
-      },
-    ],
-    createdAt: new Date("2023-10-10T10:10:10.000Z"),
-  },
-  {
-    _id: "2",
-    title: "How to center a div?",
-    tags: [
-      { _id: "3", name: "css" },
-      { _id: "4", name: "html" },
-    ],
-    author: {
-      _id: "1",
-      name: "John Doe",
-      picture: "https://example.com/johndoe.jpg",
-    },
-    upvotes: 15,
-    views: 150,
-    answers: [
-      {
-        _id: "2",
-        content: "You can use the 'margin: 0 auto;' technique.",
-        author: {
-          _id: "3",
-          name: "Sam Brown",
-          picture: "https://example.com/sambrown.jpg",
-        },
-      },
-    ],
-    createdAt: new Date("2021-10-10T10:10:10.000Z"),
-  },
-];
+export default async function Home() {
+  const result = await getQuestions({});
 
-export default function Home() {
+  console.log(result.questions);
+
   return (
     <>
       <div className="flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center">
@@ -95,8 +43,8 @@ export default function Home() {
       <HomeFilters />
 
       <div className="mt-10 flex w-full flex-col gap-6">
-        {questions.length > 0 ? (
-          questions.map((question) => (
+        {result.questions.length > 0 ? (
+          result.questions.map((question) => (
             <QuestionCard
               key={question._id}
               _id={question._id}
