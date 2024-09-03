7f87c2fb8d0f17d816a2b978f205bcf651b9f52f
Derek Kahongo <derek.elijah@gmail.com>
2024-09-03 22:17:24 +0100

    tags pages creation

--- components/cards/UserCard.tsx
+++ components/cards/UserCard.tsx
@@ -17,41 +17,43 @@ interface Props {
 const UserCard = async ({ user }: Props) => {
   const interctedTags = await getTopInteractedTag({ userId: user._id });
   return (
-    <Link
-      href={`/profile/${user.clerkId}`}
-      className="shadow-light100_darknone w-full max-xs:min-w-full xs:w-[260px]"
-    >
-      <article className="background-light900_dark200 light-border flex w-full flex-col items-center justify-center rounded-2xl border p-8">
-        <Image
-          src={user.picture}
-          alt="user profile picture"
-          width={100}
-          height={100}
-          className="rounded-full"
-        />
+    <>
+      <Link
+        href={`/profile/${user.clerkId}`}
+        className="shadow-light100_darknone w-full max-xs:min-w-full xs:w-[260px]"
+      >
+        <article className="background-light900_dark200 light-border flex w-full flex-col items-center justify-center rounded-2xl border p-8">
+          <Image
+            src={user.picture}
+            alt="user profile picture"
+            width={100}
+            height={100}
+            className="rounded-full"
+          />
 
-        <div className="mt-4 text-center">
-          <h3 className="h3-bold text-dark200_light900 line-clamp-1">
-            {user.name}
-          </h3>
-          <p className="body-regular text-dark500_light500 mt-2">
-            @{user.username}
-          </p>
-        </div>
+          <div className="mt-4 text-center">
+            <h3 className="h3-bold text-dark200_light900 line-clamp-1">
+              {user.name}
+            </h3>
+            <p className="body-regular text-dark500_light500 mt-2">
+              @{user.username}
+            </p>
+          </div>
 
-        <div className="mt-5">
-          {interctedTags.length > 0 ? (
-            <div className="flex items-center gap-2">
-              {interctedTags.map((tag) => (
-                <RenderTag key={tag._id} _id={tag._id} name={tag.name} />
-              ))}
-            </div>
-          ) : (
-            <Badge>No tags yet</Badge>
-          )}
-        </div>
-      </article>
-    </Link>
+          <div className="mt-5">
+            {interctedTags.length > 0 ? (
+              <div className="flex items-center gap-2">
+                {interctedTags.map((tag) => (
+                  <RenderTag key={tag._id} _id={tag._id} name={tag.name} />
+                ))}
+              </div>
+            ) : (
+              <Badge>No tags yet</Badge>
+            )}
+          </div>
+        </article>
+      </Link>
+    </>
   );
 };
 
