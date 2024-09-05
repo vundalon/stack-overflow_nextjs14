473bee5cc60398476915f284c351d065df6b263f
Derek Kahongo <derek.elijah@gmail.com>
2024-09-05 18:55:44 +0100

    ask question form implemented

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { ClerkProvider } from "@clerk/nextjs";
 import "./globals.css";
+import "../styles/prism.css";
+
 /* eslint-disable camelcase */
 import { Inter, Space_Grotesk } from "next/font/google";
 /* eslint-enable camelcase */
